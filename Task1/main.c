b91848ac86d5f11e326154acf0f64943ffcc9005
yllke <89221113@student.upr.si>
2023-01-06 18:08:58 +0100

    commit2
    modified the moving_elevator function so that it would show "loading" floors
    
    modified the get_new_floor function so that when the user inputs something lke 34 5898 or GjhjkGJ the program prints the invalid input message(because previously it would go on with the program because it would just look at the first chararcter)

--- Task1/main.c
+++ Task1/main.c
@@ -30,12 +30,12 @@ int get_new_floor(int current_floor) {
 
         printf("\n");
         //if statement to validate the input of the user
-        if (new_floor[0] == 'A') {
+        if (new_floor[0] == 'A' && new_floor[1] == '\0') {
             printf("Bye!"); //inputting "A" ends the program
             return -1;
-        } else if (new_floor[0] == 'G') {
+        } else if (new_floor[0] == 'G' && new_floor[1] == '\0') {
             return MIN_FLOOR; //ground floor = 0
-        } else if (new_floor[0] >= '1' && new_floor[0] <= '5' ) {
+        } else if (new_floor[0] >= '1' && new_floor[0] <= '5' && new_floor[1] == '\0') {
             return new_floor[0] - '0'; //if new_floor is between 1-5 then we do decimal number of that character(from ASCII table)
                                        // minus 49, which is the decimal number of '0' in the ascii table
         } else {
@@ -49,15 +49,15 @@ int get_new_floor(int current_floor) {
 void moving_elevator(int current_floor, int new_floor) {
     if (new_floor > current_floor) {
         printf("Going up...\n");
-
-        printf("%d...\n");
-
+        for (int i = current_floor + 1; i <= new_floor; i++) {
+            printf("%d...\n", i);
+        }
 
     } else if (new_floor < current_floor) {
         printf("Going down...\n");
-
-        printf("%d...\n");
-
+        for (int i = current_floor - 1; i >= new_floor; i--) {
+            printf("%d...\n", i);
+        }
     }
     current_floor = new_floor;
 }
